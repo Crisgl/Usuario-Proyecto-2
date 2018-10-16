d6d070e3008e569062d5c0d162c38bfa32ae5cf3
Cristian <fightingblaster@hotmail.com>
2018-10-16 14:48:08 -0500

    Initial Commit

---
+++ User/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  User
+//
+//  Created by Cristian Lopez on 16/10/18.
+//  Copyright © 2018 Cristian Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
